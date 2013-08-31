9ae72a65c3eed68c29d4854e4d1b0eed6f8aef82
DebianJoe <jbpaladin27@gmail.com>
2013-08-31 04:54:34 -0500

    operators

--- basics.sh
+++ basics.sh
@@ -17,3 +17,10 @@ fi
 # -d == dir exists (the dir as operand)
 # -f == there exists a file with operand's name (operand)
 # -eq == the operands are integers and are equal (2 operands)
+# -neq == opposite of -eq (2 operands)
+# = the operands are equal [strings] (2 operands)
+# != the opposite of =
+# -lt op 1 is less than op 2 (2 operands, integers)
+# -gt op 1 is greater than op2
+# -ge == >=
+# -le == <=
